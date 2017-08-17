471b8282c9bc072c757b1cb2b6f419087733cd33
Abra.Zhang <zhangxiangyun@ixiangyun.me>
2017-08-17 11:14:53 +0800

    fix footer bottom bug

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,3 +1,3 @@
-<footer role="contentinfo">
+<footer role="contentinfo" class="navbar navbar-inverse navbar-fixed-bottom">
 	<p class="copyright">版权所有 &copy; 2017 <a href="/">Abra.Zhang 鲜鱼的罐头盒</a> 保留一切权利</p>
 </footer>
