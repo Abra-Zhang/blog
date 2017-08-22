440e0c57dab0094653f61a34b88fad80161d1b4b
Abra.Z-PC <zhangxiangyun@ixiangyun.me>
2017-08-22 23:29:22 +0800

    add is_admin auth for admin enter

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -18,7 +18,9 @@
         	</ul>
         <ul class="nav navbar-nav navbar-right">
             @if (Auth::check())
-            <li><a href="#">用户列表</a></li>
+              @if (Auth::user()->is_admin)
+              <li><a href="#">后台管理</a></li>
+              @endif
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 {{ Auth::user()->name }} <b class="caret"></b>
