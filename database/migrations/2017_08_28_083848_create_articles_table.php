2d8284eb3a096bf93d35845c3051386539fda964
Abra.Z <zhangxiangyun@ixiangyun.me>
2017-08-28 17:14:44 +0800

    add articles table and create fake articles through model factory

---
+++ database/migrations/2017_08_28_083848_create_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id')->comment('文章序号');
+            $table->string('name')->comment('文章标题');
+            $table->integer('user_id')->unsigned()->comment("作者id");
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
