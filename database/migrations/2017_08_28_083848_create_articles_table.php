fc4b360a9f033b67ac3e62716b016f8b35a55783
Abra.Z <zhangxiangyun@ixiangyun.me>
2017-08-29 10:14:11 +0800

    add index for user_id column in articles table

--- database/migrations/2017_08_28_083848_create_articles_table.php
+++ database/migrations/2017_08_28_083848_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id')->comment('文章序号');
             $table->string('name')->comment('文章标题');
-            $table->integer('user_id')->unsigned()->comment("作者id");
+            $table->integer('user_id')->unsigned()->index()->comment("作者id");
             $table->text('content')->comment("文章内容");
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
