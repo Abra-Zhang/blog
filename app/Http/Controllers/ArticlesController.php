cc64cd466f1e72a09cee378e7e0653d03dcaeddb
Abra.Z <zhangxiangyun@ixiangyun.me>
2017-08-29 15:07:32 +0800

    add articles.show blade

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -35,7 +35,7 @@ class ArticlesController extends Controller
 
     public function show(Article $article)
     {
-
+        return view('articles.show', compact('article'));
     }
 
     public function store(Request $request)
---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,14 @@
+@extends('common.default')
+@section('title', $article->name)
+
+@section('content')
+<div class="row">
+  <div class="col-md-offset-2 col-md-8">
+    <div class="col-md-12">
+      <div class="col-md-offset-2 col-md-10">
+        <h3>{{  $article->name  }}</h3>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
