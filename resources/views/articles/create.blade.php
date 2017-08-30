a02f80d96aebfa24bcc49fafdf88f447ac305034
Abra.Z <zhangxiangyun@ixiangyun.me>
2017-08-30 11:58:35 +0800

    adjust css for editor

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,8 +4,10 @@
 {!! editor_css() !!}
 
 @section('content')
-<div id="editormd_id" style="margin-top:70px">
-    <textarea name="content" style="display:none;"></textarea>
+<div class="col-md-offset-2 col-md-8">
+    <div id="editormd_id">
+        <textarea name="content" style="display:none;"></textarea>
+    </div>
 </div>
 
 @section('jshere')
