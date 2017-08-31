eef2c652f47ad707234ee1359d8c82f76bcf69b9
Abra.Z-PC <zhangxiangyun@ixiangyun.me>
2017-09-01 01:15:04 +0800

    fix index articles list css bug if has banner

--- resources/views/index/_article.blade.php
+++ resources/views/index/_article.blade.php
@@ -2,9 +2,12 @@
     @if($article->banner !== "#")
     <img class="banner" src="{{  $article->banner  }}" />
     @endif
+    <div class="info">
     <img  class="gravatar"  src="{{  $article->user->gravatar(50)  }}" />
     <span class="name">{{  $article->user->name  }}</span>
     <span class="time">{{  $article->created_at  }}</span>
 	<a href="{{ route('articles.show', $article->id )}}" class="title" target="_blank">{{ $article->title }}</a>
     <span class="abstract">{{  $article->abstract  }}</span>
+    </div>
+    <div class="clearfix"></div>
 </li>
