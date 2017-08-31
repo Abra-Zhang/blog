50258c73d3fc37972ab5c92e767319b23141f433
Abra.Z-PC <zhangxiangyun@ixiangyun.me>
2017-09-01 02:29:55 +0800

    add jumbotron, and index banner image

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,3 +1,3 @@
 <footer role="contentinfo">
-	<p class="copyright">版权所有 &copy; 2017 <a href="/">Abra.Zhang - “Abra 的罐头盒”</a> 保留一切权利</p>
+	<p class="copyright">版权所有 &copy; 2017 <a href="/">Abra.Zhang - “Abra.Z Blog”</a> 保留一切权利</p>
 </footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,16 @@
 @extends('common.default')
 
 @section('content')
+<div class="jumbotron" style="background-image: url(/static/images/index_banner.jpg);background-position: center ;">
+  <h1 style="text-align: center;">Abra.Z Blog</h1>
+  <p style="text-align: center;">简单生活，简单说</p>
+</div>
 <div class="col-md-offset-2 col-md-8">
-	<div class="center-block">
 	<ol class="index-articles">
 	  @foreach ($articles as $article)
 	    @include('index._article')
 	  @endforeach
 	</ol>
 	{!! $articles->render() !!}
-	</div>
 </div>
 @stop
