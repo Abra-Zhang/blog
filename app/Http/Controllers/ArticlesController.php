28eb343fe2ec0fc678e4b10ab6ed41412207b2c0
Arnold.Z <arnold_zhang@ixiangyun.me>
2018-02-20 23:47:13 +0800

    finish articles-container

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,4 +9,8 @@ class ArticlesController extends Controller
     public function index(){
         return 'articles index';
     }
+
+    public function show(Request $request){
+        return 'article show id: ' . $request->article_id;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,10 @@
 
 Route::get('/', 'HomeController@home')->name('home');
 Route::get('/about', 'HomeController@about')->name('about');
+
+// 文章相关路由
 Route::get('/articles', 'ArticlesController@index')->name('articles');
+Route::get('/article/{article_id}', 'ArticlesController@show')->name('article.show');
+
+// 标签相关路由
 Route::get('/tags', 'TagsController@index')->name('tags');
