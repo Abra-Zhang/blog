241f324a276fabaa6c74c8950e7acd1cbd259d57
Arnold.Z <arnold_zhang@ixiangyun.me>
2018-02-20 23:12:12 +0800

    adjust banner css about screen-width = 768px

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,5 +15,47 @@
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="row">
+                <div class="
+                col-xl-8 offset-xl-1
+                col-lg-8 offset-lg-1
+                col-md-8 offset-md-1
+                col-sm-12
+                col-12
+                articles-container
+                ">
+                    <div class="post-preview">
+                        <a href="/2017/07/12/upgrading-eleme-to-pwa/">
+                            <h2 class="post-title">
+                                饿了么的 PWA 升级实践
+                            </h2>
+                            <h3 class="post-subtitle">
+                                Upgrading Ele.me to Progressive Web App
+                            </h3>
+                            <div class="post-content-preview">
+                                很荣幸在今年 2 月到 5 月的时间里，以顾问的身份加入饿了么，参与 PWA 的相关工作。这篇文章其实最初是在以英文写作发表在 medium 上的：Upgrading Ele.me to Progressive Web Apps，获得了一定的关注。所以也决定改写为中文版本再次分享出来，希望能对你有所帮助 ;)
+                                本文首发于 CSDN 与《程序员》2017 年 7...
+                            </div>
+                        </a>
+                        <p class="post-meta">
+                            Posted by Hux on July 12, 2017
+                        </p>
+                    </div>
+                    <hr>
+
+                </div>
+                <div class="
+                col-xl-3 offset-xl-0
+                col-lg-3 offset-lg-0
+                col-md-3 offset-md-0
+                col-sm-12
+                col-12
+                sidebar-container
+                ">
+
+                </div>
+            </div>
+        </div>
     </div>
 @stop
