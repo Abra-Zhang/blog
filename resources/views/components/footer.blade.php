c7ba223d662d74aa03e9233871643ebf2a05f3f5
Arnold.Zxy <arnold_zhang@ixiangyun.me>
2019-03-08 18:14:34 +0800

    add blog temlate statement

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,4 @@
 <footer class="blog-footer">
+    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
     <p>Copyright © {{ config('app.name', 'Laravel') }} 2018</p>
 </footer>
