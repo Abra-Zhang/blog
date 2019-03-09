56c7adb950996f45d9afab99bbda06e86b726d83
Arnold.Zxy <arnold_zhang@ixiangyun.me>
2019-03-09 17:08:35 +0800

    modify post index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,12 +4,15 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 offset-md-2 blog-list">
-                @foreach($posts as $post)
-                    <div class="blog-post">
-                        <h3><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></h3 >
-                    </div>
-                @endforeach
-
+                    <table class="table table-striped">
+                        <tbody>
+                        @foreach($posts as $post)
+                        <tr>
+                            <td><h3><a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a></h3></td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 {{ $posts->links('components.paginate') }}
             </div>
         </div>
