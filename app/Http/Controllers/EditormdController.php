d66d03f7c9b93b3cec274b7a9c53fb30ea4d3e51
Arnold <arnold_zhang@ixiangyun.me>
2019-10-21 10:45:19 +0800

    finish qiniu upload, use'zgldh/qiniu-laravel-storage'

--- app/Http/Controllers/EditormdController.php
+++ app/Http/Controllers/EditormdController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use zgldh\QiniuStorage\QiniuStorage;
 
 class EditormdController extends Controller
 {
@@ -20,6 +21,7 @@ class EditormdController extends Controller
             'message' => '未知错误',
             'url' => '',
         ];
+        $uploadType = config('editormd.upload_type');
         // 判断是否有上传图片
         if ($request->hasFile('editormd-image-file')){
 
@@ -35,16 +37,41 @@ class EditormdController extends Controller
         	if ($validator->passes()) {
         		$dirPath = config("editormd.upload_path");
         		$uploadPath = $dirPath . date('Ymd', time());
-        		// 若该路径没有文件夹，则创建
-        		if(!is_dir($uploadPath)) {
-        			mkdir($uploadPath, 0777, true);
-        		}
         		$ext = $file->getClientOriginalExtension();
         		if (in_array($ext, ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'webp'])) {
-        			$savePath = '/' . $uploadPath . '/' . uniqid() . '_' . date('s') . '.' . $ext;
-                    if ($file->isValid()) {
-                        $file->move($uploadPath, $savePath);
-                        $json = array_replace($json, ['success' => 1, 'url' => $savePath]);
+        			if ($file->isValid()) {
+                        switch ($uploadType) {
+                        	case 'local':
+				        		// 若该路径没有文件夹，则创建
+				        		if(!is_dir($uploadPath)) {
+				        			mkdir($uploadPath, 0777, true);
+				        		}
+                        		$savePath = '/' . $uploadPath . '/' . uniqid() . '_' . date('s') . '.' . $ext;
+                        		$file->move($uploadPath, $savePath);
+                        		$json = array_replace($json, ['success' => 1, 'url' => $savePath]);
+                        		break;
+                        	
+                        	case 'qiniu':
+                        		$disk = QiniuStorage::disk('qiniu');
+                        		$savePath = $uploadPath . '/' . uniqid() . '_' . date('s') . '.' . $ext;
+                        		$result = $disk->put($savePath, file_get_contents($file));;
+                        		if ($result) {
+	                        		if (config("editormd.upload_http") === 'https') {
+			                            $realurl = $disk->downloadUrl($savePath, 'https');
+			                        } else {
+			                            $realurl = $disk->downloadUrl($savePath);
+			                        }
+			                        $json = array_replace($json, ['success' => 1, 'url' => $realurl]);
+                        		}else {
+                        			$json = array_replace($json, ['success' => 0, 'meassge' => '上传七牛失败']);
+                        		}
+
+                        		break;
+
+                        	default:
+                        		# code...
+                        		break;
+                        }
                     } else {
                         $json = array_replace($json, ['success' => 0, 'message' => '文件校验失败']);
                     }
