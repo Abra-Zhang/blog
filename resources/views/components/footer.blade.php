90571f30ca69cf01d6c461ada3f04ade523623ff
Arnold <register@ixiangyun.me>
2019-11-14 16:29:09 +0800

    modify footer copyright

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="blog-footer">
     <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    <p>Copyright © {{ config('app.name', 'Laravel') }} 2018</p>
+    <p>Copyright © {{ config('app.name', 'Laravel') }} 2016 - {{ date('Y', time()) }}</p>
 </footer>
