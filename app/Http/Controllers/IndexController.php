135b9b30e59a96b5c297a8b342a60d860c4d7025
Arnold <arnold_zhang@ixiangyun.me>
2019-12-07 15:15:19 +0800

    finish blog index using redis

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = Post::where('status', '1')->orderBy('created_at', 'desc')->orderBy('id', 'desc')->simplePaginate(5);
+        //可用文章（已发布文章）List key
+        $listKey = "LIST:POSTS_ABLE";
+        // 文章Hash key
+        $hashKey = "HASH:POST";
+
+        // 先检查Redis中是否存在可用文章
+        if (Redis::exists($listKey)) {
+            $lists = Redis::lrange($listKey, 0, -1);
+            foreach ($lists as $postID) {
+                $posts[] = Redis::hgetall($hashKey . $postID);
+            }
+        } else {
+            // 若没有则从数据库中获取可用文章，并存如Redis
+            $posts = Post::where('status', '1')->orderBy('created_at', 'desc')->orderBy('id', 'desc')->get();
+            //处理文章作者名以及文章hashID
+            foreach ($posts as $post) {
+                $post['author'] = $post->user->name;
+                $post['routeKey'] = $post->getRouteKey();
+            }
+            $posts = $posts->toArray();
+
+            //将可用文章以及可用id存入Redis
+            foreach ($posts as $post) {
+                Redis::rpush($listKey, $post['id']);
+                Redis::hmset($hashKey . $post['id'], $post);
+            }
+        }
+
         return view('index', [
             'posts' => $posts,
         ]);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,78 +2,79 @@
 
 @section('content')
 
-    <main role="main" class="container">
-        <div class="row">
-            <div class="col-md-8 blog-main">
+<main role="main" class="container">
+    <div class="row">
+        <div class="col-md-8 blog-main">
 
-                @foreach($posts as $i => $post)
-                    <div class="blog-post">
-                        <h2 class="blog-post-title"><a href="{{ route('posts.show', $post->getRouteKey()) }}">{{ $post->title }}</a></h2>
-                        <p class="blog-post-meta">{{ $post->created_at }} by {{ $post->user->name }}</p>
+            @foreach($posts as $i => $post)
+            <div class="blog-post">
+                <h2 class="blog-post-title"><a href="{{ route('posts.show', $post['routeKey']) }}">{{ $post['title'] }}</a></h2>
+                <p class="blog-post-meta">{{ $post['created_at'] }} by {{ $post['author'] }}</p>
 
-                        <div id="post-{{ $i }}">
-                            <textarea class="form-control" name="content" style="display:none;">{{ Str::limit($post->content, 300, ' ... ') }}</textarea>
-                        </div>
-                        <a href="{{ route('posts.show', $post->getRouteKey()) }}">阅读更多</a>
-                    </div><!-- /.blog-post -->
-                @endforeach
-                {{ $posts->links('components.paginate') }}
+                <div id="post-{{ $i }}">
+                    <textarea class="form-control" name="content" style="display:none;">{{ Str::limit($post['content'], 300, ' ... ') }}</textarea>
+                </div>
+                <a href="{{ route('posts.show', $post['routeKey']) }}">阅读更多</a>
+            </div>
+            <!-- /.blog-post -->
+            @endforeach
 
-            </div><!-- /.blog-main -->
 
-            <aside class="col-md-4 blog-sidebar">
-                {{--<div class="p-4 mb-3 bg-light rounded">--}}
-                    {{--<h4 class="font-italic">About</h4>--}}
-                    {{--<p class="mb-0"></p>--}}
-                {{--</div>--}}
+        </div><!-- /.blog-main -->
 
-                {{--<div class="p-4">--}}
-                    {{--<h4 class="font-italic">Archives</h4>--}}
-                    {{--<ol class="list-unstyled mb-0">--}}
-                        {{--<li><a href="#">March 2014</a></li>--}}
-                        {{--<li><a href="#">February 2014</a></li>--}}
-                        {{--<li><a href="#">January 2014</a></li>--}}
-                        {{--<li><a href="#">December 2013</a></li>--}}
-                        {{--<li><a href="#">November 2013</a></li>--}}
-                        {{--<li><a href="#">October 2013</a></li>--}}
-                        {{--<li><a href="#">September 2013</a></li>--}}
-                        {{--<li><a href="#">August 2013</a></li>--}}
-                        {{--<li><a href="#">July 2013</a></li>--}}
-                        {{--<li><a href="#">June 2013</a></li>--}}
-                        {{--<li><a href="#">May 2013</a></li>--}}
-                        {{--<li><a href="#">April 2013</a></li>--}}
-                    {{--</ol>--}}
-                {{--</div>--}}
-            </aside><!-- /.blog-sidebar -->
+        <aside class="col-md-4 blog-sidebar">
+            {{--<div class="p-4 mb-3 bg-light rounded">--}}
+            {{--<h4 class="font-italic">About</h4>--}}
+            {{--<p class="mb-0"></p>--}}
+            {{--</div>--}}
 
-        </div><!-- /.row -->
+            {{--<div class="p-4">--}}
+            {{--<h4 class="font-italic">Archives</h4>--}}
+            {{--<ol class="list-unstyled mb-0">--}}
+            {{--<li><a href="#">March 2014</a></li>--}}
+            {{--<li><a href="#">February 2014</a></li>--}}
+            {{--<li><a href="#">January 2014</a></li>--}}
+            {{--<li><a href="#">December 2013</a></li>--}}
+            {{--<li><a href="#">November 2013</a></li>--}}
+            {{--<li><a href="#">October 2013</a></li>--}}
+            {{--<li><a href="#">September 2013</a></li>--}}
+            {{--<li><a href="#">August 2013</a></li>--}}
+            {{--<li><a href="#">July 2013</a></li>--}}
+            {{--<li><a href="#">June 2013</a></li>--}}
+            {{--<li><a href="#">May 2013</a></li>--}}
+            {{--<li><a href="#">April 2013</a></li>--}}
+            {{--</ol>--}}
+            {{--</div>--}}
+        </aside><!-- /.blog-sidebar -->
 
-    </main><!-- /.container -->
+    </div><!-- /.row -->
+
+</main><!-- /.container -->
 @endsection
 @section('js')
-    <script src="/vendor/editormd/js/editormd.min.js"></script>
-    <script src="/vendor/editormd/lib/marked.min.js"></script>
-    <script src="/vendor/editormd/lib/prettify.min.js"></script>
-    <script src="/vendor/editormd/lib/raphael.min.js"></script>
-    <script src="/vendor/editormd/lib/underscore.min.js"></script>
-    <script src="/vendor/editormd/lib/sequence-diagram.min.js"></script>
-    <script src="/vendor/editormd/lib/flowchart.min.js"></script>
-    <script src="/vendor/editormd/lib/jquery.flowchart.min.js"></script>
-    <script type="text/javascript">
-        // 首页博客数组
-        var posts = @json($posts).data;
-        $(document).ready(function() {
-            $.each(posts, function(index, post){
-                var wordsView;
-                wordsView = editormd.markdownToHTML("post-"+ index, {
-                    htmlDecode      : "style,script,iframe",  // you can filter tags decode
-                    emoji           : true,
-                    taskList        : true,
-                    tex             : true,  // 默认不解析
-                    flowChart       : true,  // 默认不解析
-                    sequenceDiagram : true,  // 默认不解析
-                });
-            })
+<script src="/vendor/editormd/js/editormd.min.js"></script>
+<script src="/vendor/editormd/lib/marked.min.js"></script>
+<script src="/vendor/editormd/lib/prettify.min.js"></script>
+<script src="/vendor/editormd/lib/raphael.min.js"></script>
+<script src="/vendor/editormd/lib/underscore.min.js"></script>
+<script src="/vendor/editormd/lib/sequence-diagram.min.js"></script>
+<script src="/vendor/editormd/lib/flowchart.min.js"></script>
+<script src="/vendor/editormd/lib/jquery.flowchart.min.js"></script>
+<script type="text/javascript">
+    // 首页博客数组
+    var posts = @json($posts);
+    $(document).ready(function() {
+        $.each(posts, function(index, post) {
+            var wordsView;
+            wordsView = editormd.markdownToHTML("post-" + index, {
+                htmlDecode: "style,script,iframe", // you can filter tags decode
+                emoji: true,
+                taskList: true,
+                tex: true, // 默认不解析
+                flowChart: true, // 默认不解析
+                sequenceDiagram: true, // 默认不解析
+            });
         })
-    </script>
+    })
+</script>
 @endsection
