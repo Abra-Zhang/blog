812922be83107f5d1dc3fdd8f54b82f54b119da8
Arnold <arnold_zhang@ixiangyun.me>
2019-12-08 13:55:25 +0800

    use redis in post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class PostController extends Controller
 {
@@ -27,7 +28,37 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::where('status', '1')->orderBy('created_at', 'desc')->orderBy('id', 'desc')->simplePaginate(10);
+        // $posts = Post::where('status', '1')->orderBy('created_at', 'desc')->orderBy('id', 'desc')->simplePaginate(10);
+
+
+        //可用文章（已发布文章）List key
+        $listKey = "LIST:POSTS_ABLE";
+        // 文章Hash key
+        $hashKey = "HASH:POST";
+
+        // 先检查Redis中是否存在可用文章
+        if (Redis::exists($listKey)) {
+            $lists = Redis::lrange($listKey, 0, -1);
+            foreach ($lists as $postID) {
+                $posts[] = Redis::hgetall($hashKey . $postID);
+            }
+        } else {
+            // 若没有则从数据库中获取可用文章，并存如Redis
+            $posts = Post::where('status', '1')->orderBy('created_at', 'desc')->orderBy('id', 'desc')->get();
+            //处理文章作者名以及文章hashID
+            foreach ($posts as $post) {
+                $post['author'] = $post->user->name;
+                $post['routeKey'] = $post->getRouteKey();
+            }
+            $posts = $posts->toArray();
+
+            //将可用文章以及可用id存入Redis
+            foreach ($posts as $post) {
+                Redis::rpush($listKey, $post['id']);
+                Redis::hmset($hashKey . $post['id'], $post);
+            }
+        }
+
         return view('posts.index', [
             'posts' => $posts
         ]);
@@ -55,7 +86,17 @@ class PostController extends Controller
         $post->title = $request->input('title');
         $post->content = $request->input('content');
         $post->user_id = $request->input('userId');
+
         if ($post->save()) {
+
+            $post->author = $post->user->name;
+            $post->routeKey = $post->getRouteKey();
+            $post = $post->toArray();
+
+            // 文章Hash key
+            $hashKey = "HASH:POST";
+            Redis::hmset($hashKey . $post['id'], $post);
+
             return ['code' => 0, 'msg' => 'success'];
         } else {
             return ['code' => 1, 'msg' => 'fail'];
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.app')
 
 @section('css')
-    <style>
-        .table td {
-            padding: 1.75rem!important;
-        }
-    </style>
+<style>
+    .table td {
+        padding: 1.75rem !important;
+    }
+</style>
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2 blog-list">
-                    <table class="table table-striped">
-                        <tbody>
-                        @foreach($posts as $post)
-                        <tr>
-                            <td><h4><a href="{{ route('posts.show', $post->getRouteKey()) }}">{{ $post->title }}</a></h4></td>
-                        </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                {{ $posts->links('components.paginate') }}
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2 blog-list">
+            <table class="table table-striped">
+                <tbody>
+                    @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <h4><a href="{{ route('posts.show', $post['routeKey']) }}">{{ $post['title'] }}</a></h4>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
